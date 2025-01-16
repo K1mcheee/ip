d01196d974c8a7906c6fc7bb2fdb7f023288f291
K1mcheee <kimberlychiengyj@gmail.com>
2025-01-16 19:15:06 +0800

    Add greet function

--- src/main/java/Mocha.java
+++ src/main/java/Mocha.java
@@ -1,6 +1,9 @@
 public class Mocha {
+    public static final String BR = "____________________________________________________________";
 
     public static void main(String[] args) {
-
+        System.out.println(BR + "\n Hello! I'm Mocha");
+        System.out.println(" What can I do for you? \n" + BR);
+        System.out.println(" Bye. Hope to see you again soon! \n" + BR);
     }
 }
