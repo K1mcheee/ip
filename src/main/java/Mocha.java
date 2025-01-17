14ff31f72bf7dc0d9b4c0ecbe944ff694bb7f2a7
K1mcheee <kimberlychiengyj@gmail.com>
2025-01-17 20:41:09 +0800

    Add echo functionality; fix access modifier of BR

--- src/main/java/Mocha.java
+++ src/main/java/Mocha.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Mocha {
-    public static final String BR = "____________________________________________________________";
+    private static final String BR = "____________________________________________________________";
+    private static boolean isRunning = true;
 
     public static void main(String[] args) {
         System.out.println(BR + "\n Hello! I'm Mocha");
         System.out.println(" What can I do for you? \n" + BR);
-        System.out.println(" Bye. Hope to see you again soon! \n" + BR);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (isRunning) {
+            String input = scanner.nextLine();
+            if (input.toLowerCase().equals("bye")) {
+                isRunning = false;
+                System.out.println(BR + "\n Bye. Hope to see you again soon! \n" + BR);
+                break;
+            }
+            System.out.println(BR + "\n" + input + "\n" + BR);
+        }
+
     }
 }
