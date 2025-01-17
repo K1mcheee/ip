6ac1b4f56c4b8a99394e0b39710ff1518ee3b881
K1mcheee <kimberlychiengyj@gmail.com>
2025-01-17 21:31:32 +0800

    Added mark, unmark and toString functions to Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -6,4 +6,31 @@ public class Task {
         this.name = name;
     }
 
+    /**
+     * Marks task as done and prints out status of task
+     */
+    public void mark() {
+        this.isDone = true;
+        System.out.println(" Nice! I've marked this task as done:");
+        System.out.println(this.toString());
+    }
+
+    /**
+     * Marks task as not done yet and print out status of task
+     */
+    public void unmark() {
+        this.isDone = false;
+        System.out.println(" OK, I've marked this task as not done yet:");
+        System.out.println(this.toString());
+    }
+    /**
+     * Checks whether tasks is done and return accordingly
+     * @return Task name and indication of status
+     */
+    @Override
+    public String toString() {
+        String mark = this.isDone ? "X" : " ";
+        return String.format("[%s] %s", mark, this.name);
+    }
+
 }
