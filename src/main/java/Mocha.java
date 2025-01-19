b63d622e1e4d272bd4d1eec37a4217006ebeb2a5
K1mcheee <kimberlychiengyj@gmail.com>
2025-01-19 22:26:02 +0800

    Add exception handling with custom messages

--- src/main/java/Mocha.java
+++ src/main/java/Mocha.java
@@ -24,6 +24,115 @@ public class Mocha {
         return String.format(" Now you have %d tasks in the list. \n", commands.size());
     }
 
+    private static void validateInput(String input) throws MochaException {
+
+        // parse input
+        String[] split = input.split(" ");
+        String[] date = input.split("/");
+        String tmp = split[0].toLowerCase();
+
+
+
+        // check for keywords mark and unmark
+        if (tmp.equals("mark") || tmp.equals("unmark")) {
+            int idx = Integer.parseInt(split[1]);
+
+            if (idx < 1 || idx > commands.size()) {
+                throw new MochaException("Task does not exist in the list! Try a different number");
+            }
+
+            if (tmp.equals("mark")) {
+                System.out.println(BR);
+                commands.get(idx - 1).mark();
+                System.out.println(BR);
+            }
+            if (tmp.equals("unmark")) {
+                System.out.println(BR);
+                commands.get(idx - 1).unmark();
+                System.out.println(BR);
+            }
+        } else {
+            // templates for string printing
+            String name = "";
+            String dueDate = "";
+            String to = "";
+            String from = "";
+
+            // retrieves command at 0 idx of array before dueDates
+            String[] cmd = date[0].split(" ");
+
+            // switch case to respond to input
+            switch (tmp) {
+                case "todo":
+                    if (split.length < 2) {
+                        MochaException.emptyDescription("todo have 5 cups of bubble tea");
+                    }
+                    // retrieve task
+                    for (int i = 1; i < split.length; i++) {
+                        name += " " + split[i];
+                    }
+                    task = new Todo(name);
+                    commands.add(task);
+                    System.out.println(BR + printNew() + task + "\n" + printUpdates() + BR);
+                    break;
+                case "deadline":
+                    if (split.length < 2) {
+                        MochaException.emptyDescription("deadline finish the bubble tea /by today");
+                    }
+                    if (date.length < 2) {
+                        throw new MochaException("Remember to add a due date using:\n /by duedate");
+                    }
+                    // retrieve task
+                    for (int i = 1; i < cmd.length; i++) {
+                        name += " " + cmd[i];
+                    }
+
+                    // retrieve deadline
+                    String[] byWhen = date[1].split(" ");
+                    for (int i = 1; i < byWhen.length; i++) {
+                        dueDate += " " + byWhen[i];
+                    }
+
+                    task = new Deadline(name, dueDate);
+                    commands.add(task);
+                    System.out.println(BR + printNew() + task + "\n" + printUpdates() + BR);
+                    break;
+
+                case "event":
+                    if (split.length < 2) {
+                        MochaException.emptyDescription("event Try all bubble teas /from Monday /to Friday");
+                    }
+                    if (date.length < 3) {
+                        throw new MochaException("events require a from and to date!\n /from fromDate /to toDate");
+                    }
+                    // retrieve task
+                    for (int i = 1; i < cmd.length; i++) {
+                        name += " " + cmd[i];
+                    }
+
+                    // retrieve fromDate
+                    String[] fromDate = date[1].split(" ");
+                    for (int i = 1; i < fromDate.length; i++) {
+                        from += " " + fromDate[i];
+                    }
+
+                    // retrieve toDate
+                    String[] toDate = date[2].split(" ");
+                    for (int i = 1; i < toDate.length; i++) {
+                        to += " " + toDate[i];
+                    }
+
+                    task = new Event(name, from, to);
+                    commands.add(task);
+                    System.out.println(BR + printNew() + task + "\n" + printUpdates() + BR);
+                    break;
+
+                default:
+                    throw new MochaException("Searching through tasks... \nsorry, it seems I have not learnt this command!");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(BR + "\n Hello! I'm Mocha");
         System.out.println(" What can I do for you? \n" + BR);
@@ -46,95 +155,15 @@ public class Mocha {
                 }
                 System.out.println(BR);
             } else {
-                // parse input
-                String[] split = input.split(" ");
-                String[] date = input.split("/");
-                String tmp = split[0].toLowerCase();
 
-
-                // check for keywords mark and unmark
-                if (tmp.equals("mark") || tmp.equals("unmark")) {
-                    int idx = Integer.parseInt(split[1]);
-
-                    if (idx < 1 || idx > commands.size()) {
-                        throw new IndexOutOfBoundsException("Task does not exist");
-                    }
-
-                    if (tmp.equals("mark")) {
-                        System.out.println(BR);
-                        commands.get(idx - 1).mark();
-                        System.out.println(BR);
-                    }
-                    if (tmp.equals("unmark")) {
-                        System.out.println(BR);
-                        commands.get(idx - 1).unmark();
-                        System.out.println(BR);
-                    }
-                } else {
-                    // templates for string printing
-                    String name = "";
-                    String dueDate = "";
-                    String to = "";
-                    String from = "";
-
-                    // retrieves command at 0 idx of array before dueDates
-                    String[] cmd = date[0].split(" ");
-
-                    // switch case to respond to input
-                    switch (tmp) {
-                        case "todo":
-                            // retrieve task
-                            for (int i = 1; i < split.length; i++) {
-                                name += " " + split[i];
-                            }
-                            task = new Todo(name);
-                            commands.add(task);
-                            System.out.println(BR + printNew() + task + "\n" + printUpdates() + BR);
-                            break;
-                        case "deadline":
-                            // retrieve task
-                            for (int i = 1; i < cmd.length; i++) {
-                                name += " " + cmd[i];
-                            }
-
-                            // retrieve deadline
-                            String[] byWhen = date[1].split(" ");
-                            for (int i = 1; i < byWhen.length; i++) {
-                                dueDate += " " + byWhen[i];
-                            }
-
-                            task = new Deadline(name, dueDate);
-                            commands.add(task);
-                            System.out.println(BR + printNew() + task + "\n" + printUpdates() + BR);
-                            break;
-
-                        case "event":
-                            // retrieve task
-                            for (int i = 1; i < cmd.length; i++) {
-                                name += " " + cmd[i];
-                            }
-
-                            // retrieve fromDate
-                            String[] fromDate = date[1].split(" ");
-                            for (int i = 1; i < fromDate.length; i++) {
-                                from += " " + fromDate[i];
-                            }
-
-                            // retrieve toDate
-                            String[] toDate = date[2].split(" ");
-                            for (int i = 1; i < toDate.length; i++) {
-                                to += " " + toDate[i];
-                            }
-
-                            task = new Event(name, from, to);
-                            commands.add(task);
-                            System.out.println(BR + printNew() + task + "\n" + printUpdates() + BR);
-                            break;
-
-                        default:
-                            throw new IllegalArgumentException("Invalid command");
-                    }
+                try {
+                    validateInput(input);
+                } catch (MochaException e) {
+                    System.out.println(BR);
+                    System.out.println(e.getMessage());
+                    System.out.println(BR);
                 }
+
 
             }
 
--- src/main/java/MochaException.java
+++ src/main/java/MochaException.java
@@ -1,5 +1,10 @@
 public class MochaException extends Exception {
+
     public MochaException(String message) {
         super(message);
+    }
+
+    public static void emptyDescription(String task) throws MochaException{
+        throw new MochaException("Task requires a description!\nFor example...\n" + task);
     }
 }
